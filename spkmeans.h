2ae66c116bb1d0dbf9c4d782ad4ecc18f4378139
yotammaoz <80283596+yotammaoz@users.noreply.github.com>
2022-09-15 14:46:06 +0300

    Update spkmeans.h

--- spkmeans.h
+++ spkmeans.h
@@ -4,7 +4,6 @@
 int get_dimension_from_file(char *filename);
 int get_num_of_points_from_file(char *filename);
 void read_data_from_input_file_to_matrix(double **mat, char *filename);
-void print_mat(int d, int n, double **mat);
 void run_goal(char *, char *);
 struct k_n_and_t_matrix *calc_k_n_and_t(char *file_name, int k);
 
